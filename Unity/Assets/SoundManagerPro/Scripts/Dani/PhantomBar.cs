b8e2273e0fff2b805eec866debb19707a07a27d5
Dani Felix Montoya Laynes <totallynotabot@gmail.com>
2020-02-29 10:30:09 +0100

    UIAndEnemySprite

---
+++ Unity/Assets/SoundManagerPro/Scripts/Dani/PhantomBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhantomBar : MonoBehaviour
+{
+	
+	public float phantomEnergy = 100;
+	public float phantomEnergyMax = 100;
+	
+	public Image phantomEnergyImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        phantomEnergy = 100;
+		phantomEnergyMax = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//DEBUG
+		phantomEnergyImage.fillAmount = phantomEnergy / phantomEnergyMax;
+        if(Input.GetKeyDown(KeyCode.A))
+		{
+			phantomEnergy -= 10;
+		}
+    }
+}

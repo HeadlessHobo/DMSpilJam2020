ff49c01c6b0646eebe5d0659620f1dfc26fdd58f
Dani Felix Montoya Laynes <totallynotabot@gmail.com>
2020-02-29 12:30:06 +0100

    AnimationsNShit

---
+++ Unity/Assets/Scripts/Dani/EnemyAnim.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnim : MonoBehaviour
+{
+	
+	public Animator EnemyAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+		{
+			IdleBlink();
+		}
+    }
+	
+	public void AnimShoot()
+	{
+		EnemyAnimator.Play("ShootPlayer");
+	}
+	
+	public void AnimEnDeath()
+	{
+		
+		EnemyAnimator.Play("EnemyDeath");
+	}
+	
+	public void AnimEnIdle()
+	{
+		EnemyAnimator.Play("IdleEnemey");
+	}
+	
+	public void IdleBlink()
+	{
+		EnemyAnimator.Play("IdleBlink");
+	}
+}

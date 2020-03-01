5dcdc1ea6dc58a69eaec8eda009e6b060653d523
Dani Felix Montoya Laynes <totallynotabot@gmail.com>
2020-03-01 14:05:51 +0100

    TilesANdSound

---
+++ Unity/Assets/Scripts/Dani/SoundManagerDefault.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagerDefault : MonoBehaviour
+{
+	
+	public AudioClip MusicSound;
+	public AudioSource MusicSoundSource;
+	
+	public AudioClip JumpSound;
+	public AudioSource JumpSoundSource;
+	
+	public AudioClip PlayerLandSound;
+	public AudioSource PlayerLandSoundSource;
+	
+
+	
+	public AudioClip MonsterDeathSound;
+	public AudioSource MonsterDeathSoundSource;
+	
+	public AudioClip MonsterShootSound;
+	public AudioSource MonsterShootSoundSource;
+	
+	public AudioClip PortalSound;
+	public AudioSource PortalSoundSource;
+
+	public AudioClip ProjectileHitSound;
+	public AudioSource ProjectileHitSoundSource;
+	
+	public AudioClip PlayerDeathSound;
+	public AudioSource PlayerDeathSoundSource;
+	
+	public AudioClip EnterPhantSound;
+	public AudioSource EnterPhantSoundSource;
+	
+	public AudioClip ExitPhantSound;
+	public AudioSource ExitPhantSoundSource;
+	
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	public void PlayJumpSound()
+	{
+		
+	}
+	
+	public void PlayLandingSound()
+	{
+		
+	}
+	
+	public void PlayPortalSound()
+	{
+		
+	}
+	
+	public void PlayMonsterDeathSound()
+	{
+		
+	}
+	
+	public void PlayMonsterShootSound()
+	{
+		
+	}
+	
+	public void PlayPlayerDeathSound()
+	{
+		
+	}
+	
+	public void PlayEntPhantSound()
+	{
+		
+	}
+	
+	public void PlayExitPhantSound()
+	{
+		
+	}
+	
+}
